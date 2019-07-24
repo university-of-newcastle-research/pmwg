9280893bf0441f6d048bcc12351b539961c2b540
gjcooper <gjcooper@gmail.com>
2019-07-24 10:51:42 +1000

    Ignores added, pm.corl changes, pkg and func docs
    * Rbuildignore changes for lintr, packrat and .git
    * gitignroe the ..Rcheck folder
    * NAMESPACE changes built by roxygem2
    * Documentation for pm.corl
    * Name change for pm.corl (new_sample, var name changes and reformat
    * Pkg documentation additions and filename change
    * Remove add.R (generated by cookiecutter)
    * Documentation generated by roxygen2

---
+++ R/psamplers-package.R
@@ -0,0 +1,21 @@
+#' Particle Sampling Methods
+#'
+#' \tabular{ll}{
+#' Package: \tab psamplers\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.0.0.9004\cr
+#' Date: \tab 2019-07-23\cr
+#' Depends: \tab R (>= 3.5.0)\cr
+#' License: \tab GPL-3\cr
+#' URL: \tab https://github.com/gjcooper/samplers\cr
+#' }
+#'
+#' Provides particle sampling helper functions (log-likelihood functions, particle sampling functions). The initial version of the code is for parameter estimation and is primarily an implementation of the Particle Metropolis within Gibbs sampler outlined in the paper available at https://arxiv.org/abs/1806.10089 and based on the Matlab code available at http://osf.io/5b4w3
+#'
+#' @aliases psamplers-package
+#' @name psamplers
+#' @docType package
+#' @title The psamplers Package
+#' @author Gavin Cooper
+#' @keywords package
+NULL
