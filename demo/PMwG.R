445888500ea6664bc070afa1b0012b269a25fd93
gjcooper <gjcooper@gmail.com>
2019-09-19 14:25:38 +1000

    Ignore swap files, lintr fixes/additions

--- demo/PMwG.R
+++ demo/PMwG.R
@@ -1,6 +1,6 @@
 library(rtdists)
 library(mvtnorm) ## for the multivariate normal.
-library(MASS) ## for matrix inverse.
+library(MSS) ## for matrix inverse.
 library(MCMCpack) ## for the inverse wishart random numbers.
 library(psamplers)
 rm(list = ls())
@@ -20,14 +20,6 @@ pmwg_args <- list(
   "max_iterations" = 10000,
   "thin" = 1
 )
-# pmwg_args <- list(
-#   "adaptation_particles" = 1000,
-#   "burnin_iterations" = 500,
-#   "sampling_particles" = 100,
-#   "sampling_iterations" = 10000,
-#   "max_iterations" = 100000,
-#   "thin" = 1
-# )
 
 # Load Forstmann et al.'s data.
 data <- read.csv("data/data.csv", header = FALSE)
@@ -63,7 +55,8 @@ pts2 <- param_theta_sigma2[, , 1] # nolint
 
 # Start points for the population-level parameters only. Hard coded here, just
 # for convenience.
-ptm[1:num_parameters] <- start_points_mu # Weird subscripts maintains the naming.
+# Weird subscripts maintains the naming.
+ptm[1:num_parameters] <- start_points_mu
 pts2 <- start_points_sig2 # Who knows??
 # Because this is calculated near the end of the main loop, needs initialising for iter=1.
 pts2_inv <- ginv(pts2)
@@ -82,7 +75,7 @@ prior_mu_sigma2_inv <- ginv(prior_mu_sigma2)
 # as for the main resampling down below.
 particles <- array(dim = c(length(ptm), S))
 num_particles <- pmwg_args$adaptation_particles
-cat('Sampling Initial values for random effects\n')
+cat("Sampling Initial values for random effects\n")
 pb <- txtProgressBar(min = 0, max = S, style = 3)
 for (s in 1:S) {
   setTxtProgressBar(pb, s)
@@ -102,12 +95,13 @@ if (runner_args$restart) {
   cat("\nRestarting from saved run.\n")
   load(runner_args$restart_file)
   # Check a couple of things.
-  if ((dim(particles)[1] != num_parameters) || (dim(particles)[2] != S)) {
+  if ((dim(particles)[1] != num_parameters) || (dim(particles)[2] != S)) {  #nolint
     stop("Restart does not match size (subjects).")
   }
 }
 
 if (runner_args$cpus > 1) {
+  # nolint start
   # You need the suggested package for this function
   library(snowfall)
 
@@ -123,49 +117,76 @@ if (runner_args$cpus > 1) {
     "param_theta_sig2",
     "latent_theta_mu"
   ))
+  # nolint end
 }
 
 # create progress bar
 pu <- runner_args$progress_update
-pb <- txtProgressBar(min = 0, max = pmwg_args$sampling_iterations/pu, style = 3)
+pb <- txtProgressBar(
+  min = 0,
+  max = pmwg_args$sampling_iterations / pu,
+  style = 3
+)
 
 for (i in 1:pmwg_args$sampling_iterations) {
-  if (i %% pu == 0){
+  if (i %% pu == 0) {
     setTxtProgressBar(pb, i %/% pu)
   }
   # Sample population-level parameters.
   var_mu <- ginv(S * pts2_inv + prior_mu_sigma2_inv)
   mean_mu <- as.vector(var_mu %*% (pts2_inv %*% apply(particles, 1, sum)))
   chol_var_mu <- t(chol(var_mu)) # t() because I want lower triangle.
-  ptm <- rmvnorm(1, mean_mu, chol_var_mu %*% t(chol_var_mu))[1, ] # New sample for mu.
+  # New sample for mu.
+  ptm <- rmvnorm(1, mean_mu, chol_var_mu %*% t(chol_var_mu))[1, ]
   names(ptm) <- parameters
 
   theta_temp <- particles - ptm
-  # cov.temp=array(0,dim=c(num_parameters,num_parameters))
-  # for (j in 1:S) cov.temp=cov.temp+(theta.temp[,j])%*%t(theta.temp[,j])
   cov_temp <- (theta_temp) %*% (t(theta_temp))
   B_half <- 2 * v_half * diag(1 / a_half) + cov_temp
   pts2 <- riwish(k_half, B_half) # New sample for sigma.
   pts2_inv <- ginv(pts2)
 
   # Sample new mixing weights.
-  a_half <- 1 / rgamma(n = num_parameters, shape = v_shape, scale = 1 / (v_half + diag(pts2_inv) + A_half))
+  a_half <- 1 / rgamma(
+    n = num_parameters,
+    shape = v_shape,
+    scale = 1 / (v_half + diag(pts2_inv) + A_half)
+  )
 
   # Sample new particles for random effects.
   if (runner_args$cpus > 1) {
-    tmp <- sfLapply(x = 1:S, fun = new_sample, data = data, num_proposals = num_particles, mu = ptm, sig2 = pts2, particles = particles)
+    tmp <- sfLapply(  #nolint
+      x = 1:S,
+      fun = new_sample,
+      data = data,
+      num_proposals = num_particles,
+      mu = ptm,
+      sig2 = pts2,
+      particles = particles
+    )
   } else {
-    tmp <- lapply(X = 1:S, FUN = new_sample, data = data, num_proposals = num_particles, mu = ptm, sig2 = pts2, particles = particles)
+    tmp <- lapply(
+      X = 1:S,
+      FUN = new_sample,
+      data = data,
+      num_proposals = num_particles,
+      mu = ptm,
+      sig2 = pts2,
+      particles = particles
+    )
   }
   particles <- array(unlist(tmp), dim = dim(particles))
 
   # Store results.
-  latent_theta_mu[, , i] <- particles
-  param_theta_sigma2[, , i] <- pts2
+  latent_theta_mu[, , i] <- particles # nolint
+  param_theta_sigma2[, , i] <- pts2 # nolint
   param_theta_mu[, i] <- ptm
 }
 close(pb)
-if (runner_args$cpus > 1) sfStop()
+if (runner_args$cpus > 1) sfStop() # nolint
 
-save(file = runner_args$restart_file, list = c("pts2_inv", "particles", "ptm", "pts2"))
+save(
+  file = runner_args$restart_file,
+  list = c("pts2_inv", "particles", "ptm", "pts2")
+)
 save.image("data/output/PMwG.RData")
