3dcf4db0bfab4119a25ea07a25f2874210c8d131
gjcooper <gjcooper@gmail.com>
2019-09-19 13:22:51 +1000

    Break out wind/unwind functions to helper R file

--- R/fragment-adaptive-proposal-distributions.R
+++ R/fragment-adaptive-proposal-distributions.R
@@ -1,37 +1,3 @@
-#' Unwinds variance matrix to a vector
-#'
-#' Takes a variance matrix and unwind to a vector via Cholesky then log
-#'
-#' @param var_matrix A variance matrix
-#'
-#' @return The unwound matrix as a vector
-#' @examples
-#' unwind(diag(rep(1, num_parameters)))
-#' @keywords internal
-unwind <- function(var_matrix, reverse = FALSE) {
-  y <- t(chol(var_matrix))
-  diag(y) <- log(diag(y))
-  y[lower.tri(y, diag = TRUE)]
-}
-
-#' Winds a variance vector back to a vector
-#'
-#' The reverse of the unwind function, takes a variance vector and windows back into matrix
-#'
-#' @param var_vector A variance vector
-#'
-#' @return The wound vector as a matrix
-#' @examples
-#' wind(diag(rep(1, num_parameters)))
-#' @keywords internal
-wind <- function(var_vector) {
-  n <- sqrt(2 * length(var_vector) + 0.25) - 0.5 ## Dim of matrix.
-  if ((n * n + n) != (2 * length(x))) stop("Wrong sizes in unwind.")
-  out <- array(0, dim = c(n, n))
-  out[lower.tri(out, diag = TRUE)] <- var_vector
-  diag(out) <- exp(diag(out))
-  out %*% t(out)
-}
 
 #'
 
