af17486556b0dd00afb100965807cc17e261a09d
gjcooper <gjcooper@gmail.com>
2019-12-15 22:59:55 +1100

    Add docs for new args to pmwgs initialisation, code style fixes

--- R/particle_sampler.R
+++ R/particle_sampler.R
@@ -7,8 +7,15 @@
 #'   to contain at least one column called subject whose elements are unique
 #'   identifiers for each subject.
 #' @param parameters The list of parameter names to be used in the model
-#' @param pmwg_args A list containing arguments to the PMwG sampling, including
-#'   number of iterations for each stage.
+#' @param burn A vector containing the number of particles and number of
+#'   iterations respectively for the burnin phase.
+#' @param adapt A vector containing the number of particles and maximum number
+#'   of iterations for the adaptation phase.
+#' @param sample A vector containing the number of particles and number of
+#'   iterations respectively for the sampling phase.
+#' @param llfunc A log likelihood function that given a list of parameter values
+#'   and a data.frame containing subject data will return the log likelihood of
+#'   \code{x} given \code{data}. See \code{\link{lba_loglike}} for an example.
 #'
 #' @return A pmwgs object that can be run, plotted and more
 #' @examples
