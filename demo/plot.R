0999260a81e59f16cb42c8387dd0285597a1f886
gjcooper <gjcooper@gmail.com>
2020-01-23 12:08:37 +1100

    Add nicer plotting to demo

---
+++ demo/plot.R
@@ -0,0 +1,29 @@
+#library(dplyr)
+require(ggplot2)
+
+plot.pmwgs <- function(x, type="mu", pars=NULL, subjects=NULL) {
+  if (is.null(pars)) pars <- x$par_names
+  if (is.null(subjects)) subjects <- 1:x$n_subjects
+  
+  if (type == "mu") {
+    data <- x$samples$theta_mu
+    dimnames(data) <- list(x$par_names, NULL)
+    data.frame(t(data)) %>%
+      select(pars) %>%
+      mutate(iter = 1:dim(data)[2]) %>%
+      pivot_longer(-iter) %>%
+      rename(parameter=name) %>%
+      ggplot(mapping = aes(x=iter, y=value, col=parameter)) + geom_line()
+  } else if (type == "alpha") {
+    data <- x$samples$alpha
+    dimnames(data) <- list("parameter" = x$par_names, "subject" = unique(x$data$subject), "iter" = 1:dim(data)[3])
+    as.tbl_cube(data) %>%
+      as_tibble() %>%
+      dplyr::filter(parameter %in% pars) %>%
+      dplyr::filter(subject %in% subjects) %>% 
+      rename(value=data) %>% 
+      ggplot(mapping = aes(x=iter, y=value, col=parameter)) + geom_line() + facet_wrap(~subject)
+  } else {
+    stop("Unsupported plot type for pmwgs object")
+  }
+}
