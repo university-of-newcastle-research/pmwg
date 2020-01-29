dd6fada9d62ba3c185402008766e14b29a8e7f65
gjcooper <gjcooper@gmail.com>
2020-01-29 14:56:21 +1100

    Older dataset format, update dataset description

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset containing the speed or accuracy manipulation, subject ID's and
 #' reaction times from the ...
 #'
-#' @format A data frame with 2502 rows and 4 variables:
+#' @format A data frame with 15818 rows and 4 variables:
 #' \describe{
 #'   \item{subject}{subject, integer ID for each subject}
 #'   \item{rt}{rt, reaction time for each trial as a double}
