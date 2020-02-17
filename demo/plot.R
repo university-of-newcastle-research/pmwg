ba4c4915ad687eeacee1ac3436e889d5e4163b95
gjcooper <gjcooper@gmail.com>
2020-02-17 22:45:43 +1100

    Add log likelohood plotting option

--- demo/plot.R
+++ demo/plot.R
@@ -1,9 +1,9 @@
-#library(dplyr)
+require(dplyr)
 require(ggplot2)
 
 plot.pmwgs <- function(x, type="mu", pars=NULL, subjects=NULL) {
   if (is.null(pars)) pars <- x$par_names
-  if (is.null(subjects)) subjects <- 1:x$n_subjects
+  if (is.null(subjects)) subjects <- x$subjects
   
   if (type == "mu") {
     data <- x$samples$theta_mu
@@ -23,6 +23,15 @@ plot.pmwgs <- function(x, type="mu", pars=NULL, subjects=NULL) {
       dplyr::filter(subject %in% subjects) %>% 
       rename(value=data) %>% 
       ggplot(mapping = aes(x=iter, y=value, col=parameter)) + geom_line() + facet_wrap(~subject)
+  } else if (type == "ll") {
+    data <- x$samples$subj_ll
+    dimnames(data) <- list("subject" = x$subjects, "iter" = 1:dim(data)[2])
+    data.frame(t(data)) %>%
+      select(subjects) %>%
+      mutate(iter = 1:dim(data)[2]) %>%
+      pivot_longer(-iter) %>%
+      rename(parameter=name) %>%
+      ggplot(mapping = aes(x=iter, y=value, col=parameter)) + geom_line()
   } else {
     stop("Unsupported plot type for pmwgs object")
   }
