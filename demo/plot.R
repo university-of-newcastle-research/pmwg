d36628d904aff1dd4b1d470068e3ac38217cbb3d
gjcooper <gjcooper@gmail.com>
2020-02-24 16:52:32 +1100

    Add iters arg to plotting to only plot certain iterations

--- demo/plot.R
+++ demo/plot.R
@@ -1,12 +1,13 @@
 require(dplyr)
 require(ggplot2)
 
-plot.pmwgs <- function(x, type="mu", pars=NULL, subjects=NULL) {
+plot.pmwgs <- function(x, type="mu", pars=NULL, subjects=NULL, iters=NULL) {
   if (is.null(pars)) pars <- x$par_names
   if (is.null(subjects)) subjects <- x$subjects
+  if (is.null(iters)) iters <- 1:x$samples$idx
   
   if (type == "mu") {
-    data <- x$samples$theta_mu
+    data <- x$samples$theta_mu[,iters]
     dimnames(data) <- list(x$par_names, NULL)
     data.frame(t(data)) %>%
       select(pars) %>%
@@ -15,7 +16,7 @@ plot.pmwgs <- function(x, type="mu", pars=NULL, subjects=NULL) {
       rename(parameter=name) %>%
       ggplot(mapping = aes(x=iter, y=value, col=parameter)) + geom_line()
   } else if (type == "alpha") {
-    data <- x$samples$alpha
+    data <- x$samples$alpha[,,iters]
     dimnames(data) <- list("parameter" = x$par_names, "subject" = unique(x$data$subject), "iter" = 1:dim(data)[3])
     as.tbl_cube(data) %>%
       as_tibble() %>%
@@ -24,7 +25,7 @@ plot.pmwgs <- function(x, type="mu", pars=NULL, subjects=NULL) {
       rename(value=data) %>% 
       ggplot(mapping = aes(x=iter, y=value, col=parameter)) + geom_line() + facet_wrap(~subject)
   } else if (type == "ll") {
-    data <- x$samples$subj_ll
+    data <- x$samples$subj_ll[,iters]
     dimnames(data) <- list("subject" = x$subjects, "iter" = 1:dim(data)[2])
     data.frame(t(data)) %>%
       select(subjects) %>%
