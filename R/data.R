efa4f16efe3f8fd3d594038bc3dbcd38e9661a85
gjcooper <gjcooper@gmail.com>
2020-05-11 09:58:24 +1000

    Replace dataset, bump version, clean description

--- R/data.R
+++ R/data.R
@@ -3,12 +3,13 @@
 #' A dataset containing the speed or accuracy manipulation, subject ID's and
 #' reaction times from the ...
 #'
-#' @format A data frame with 2502 rows and 4 variables:
+#' @format A data frame with 15818 rows and 5 variables:
 #' \describe{
 #'   \item{subject}{subject, integer ID for each subject}
 #'   \item{rt}{rt, reaction time for each trial as a double}
-#'   \item{correct}{correct, integer code for correct or incorrect response}
-#'   \item{condition}{condition, integer code for speed or accuracy condition}
+#'   \item{condition}{condition, Factor with 3 levels for Speed, Accuracy and Neutral}
+#'   \item{stim}{stim, Factor with 2 levels for Left and Right trials}
+#'   \item{resp}{resp, Factor with 2 levels for Left and Right responses}
 #' }
 #' @source \url{https://www.jneurosci.org/content/35/6/2476}
 "forstmann"
