c004a919c2ff9dfcc5a0c75445e4b1a4827bbe2e
gjcooper <gjcooper@gmail.com>
2020-05-25 16:40:23 +1000

    Test first two functions in helpers.R

---
+++ tests/testthat/test-util.R
@@ -0,0 +1,24 @@
+test_that("Wind+Unwind", {
+  test_mat <- matrix(rnorm(16), nrow = 4, ncol = 4)
+  # Ensure matrix is positive semi-definite
+  test_mat <- test_mat %*% t(test_mat)
+  test_vec <- rnorm(10)
+  # Now run tests
+  # Test wind and unwind are complementary
+  expect_equal(test_mat, wind(unwind(test_mat)))
+  expect_equal(test_vec, unwind(wind(test_vec)))
+  # Test unwind and wind end with correct shape
+  expect_identical(length(unwind(test_mat)), 10L)
+  expect_identical(dim(wind(test_vec)), c(4L, 4L))
+})
+
+
+test_that("numbers_from_ratio", {
+  #Test error checking
+  expect_error(numbers_from_ratio(c(0.3, 0.3, 0.3)),
+               "The elements of the mix_ratio vector must sum to 1")
+  expect_error(numbers_from_ratio(c(0.5, 0.5)),
+               "mix_ratio vector must have three elements which sum to 1")
+  #Test 0 for efficient sampling returning 0 particles to select
+  expect_equal(numbers_from_ratio(c(0.5, 0.5, 0))[3], 0)
+})
