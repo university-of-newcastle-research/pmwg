5ae4a1434bd7cc07f6e4f87f4ed5dba34ea60834
gjcooper <gjcooper@gmail.com>
2020-08-19 22:46:17 +1000

    Update description, get pkg documentation from README

--- R/pmwg-package.R
+++ R/pmwg-package.R
@@ -1,24 +1,8 @@
-#' Particle Sampling Methods
-#'
-#' \tabular{ll}{
-#' Package: \tab pmwg\cr
-#' Type: \tab Package\cr
-#' Version: \tab 0.0.0.9004\cr
-#' Date: \tab 2019-07-23\cr
-#' Depends: \tab R (>= 3.5.0)\cr
-#' License: \tab GPL-3\cr
-#' URL: \tab https://github.com/newcastlecl/pmwg\cr
-#' }
-#'
-#' Provides an R implementation of the Particle Metropolis within Gibbs sampler
-#' for model parameter and random effect estimation. Based on the paper at
-#' \url{https://doi.org/10.1016/j.jmp.2020.102368} and on the Matlab code
-#' available at \url{http://osf.io/5b4w3}
-#'
-#' @aliases pmwg-package
-#' @name pmwg
-#' @docType package
-#' @title The pmwg Package
-#' @author Gavin Cooper
-#' @keywords package
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
